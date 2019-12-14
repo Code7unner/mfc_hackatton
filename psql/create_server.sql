43a998d396c8165e1f86b41a4569e7299b5b16c8
Artem <onedeadwave@gmail.com>
2019-12-15 01:28:51 +1000

    Updated sql model

--- psql/create_server.sql
+++ psql/create_server.sql
@@ -1,12 +1,12 @@
-create table if not exists server(
-    id                      varchar(255) not null,
-    name                    varchar(255) not null,
-    is_connected            boolean default false,
-    wrong_protocol          boolean default false,
-    organization_name       varchar(255) not null,
-    organization_fullname   varchar(255) not null,
-    organization_address    varchar(100) not null,
-    organization_phone      varchar(50),
-    organization_fax        varchar(100),
-    organization_email      varchar(100)
+create table if not exists servers(
+    id text not null,
+    name text not null,
+    is_connected boolean default false,
+    wrong_protocol boolean default false,
+    organization_name varchar(255) not null,
+    organization_fullname varchar(255) not null,
+    organization_address varchar(255) not null,
+    organization_phone varchar(100),
+    organization_fax varchar(100),
+    organization_email varchar(100)
 );
