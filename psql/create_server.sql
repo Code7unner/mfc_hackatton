61d70fc5eaf872bfa42803216e07537dcef84002
Maxim <cherbadzhimaxim@gmail.com>
2019-12-14 14:50:07 +1000

    fix server db schema

--- psql/create_server.sql
+++ psql/create_server.sql
@@ -3,9 +3,10 @@ create table if not exists server(
     name                    varchar(255) not null,
     is_connected            boolean default false,
     wrong_protocol          boolean default false,
-    organization_name       varchar(100) not null,
+    organization_name       varchar(255) not null,
     organization_fullname   varchar(255) not null,
-    organization_phone      varchar(20),
+    organization_address    varchar(100) not null,
+    organization_phone      varchar(50),
     organization_fax        varchar(100),
     organization_email      varchar(100)
 );
