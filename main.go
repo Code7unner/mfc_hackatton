c470dcb62c0a8ad61f87d6cd6c20f544b129696e
Maxim <cherbadzhimaxim@gmail.com>
2019-12-14 19:01:05 +1000

    fix deploying

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/mfc_hackatton/db"
 	"github.com/mfc_hackatton/parser"
 	"github.com/mfc_hackatton/scheduler"
 	"log"
-	"net/http"
 	"os"
 )
 
 func main() {
+	port := ":" + os.Getenv("PORT")
+
 	database := db.Connect()
 	storage := db.NewStorage(database)
 
@@ -21,12 +21,12 @@ func main() {
 	r.Use(gin.Logger())
 
 	// Frontend
-	r.Use(static.Serve("/", static.LocalFile("client/build", true)))
-	r.LoadHTMLGlob("client/build/*.html")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
+	//r.Use(static.Serve("/", static.LocalFile("client/build", true)))
+	//r.LoadHTMLGlob("client/build/*.html")
+	//
+	//r.GET("/", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "index.html", nil)
+	//})
 
 	// Parsing .xlsx file
 	r.GET("/api/parser", gin.WrapF(parser.Parse))
@@ -36,7 +36,7 @@ func main() {
 	r.GET("/api/statistics", gin.WrapF(storage.GetStatistics))
 
 	// Port
-	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
+	if err := r.Run(port); err != nil {
 		log.Fatal(err)
 	}
 
